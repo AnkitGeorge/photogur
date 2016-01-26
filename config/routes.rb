2010f5fb2098c68b241452acff7de9a05d1137ec
AnkitGeorge <ankit.george@gmail.com>
2016-01-26 16:02:39 -0500

    Make each picture show at its own URL with dynamic routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
 
   # Ignore the comments below for now
   # They are just documentation
